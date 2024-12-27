3880c2dea852c346e79b3aef1084dfae14f5a4e9
KopiOCoder <wesleylimkopi@gmail.com>
2024-12-27 15:56:20 +0800

    Create source_code.py

---
+++ source_code.py
@@ -0,0 +1,9 @@
+# ************************************************************************* 
+# Program: source_code.py 
+# Course: CSP1114 PROBLEM SOLVING AND PROGRAM DESIGN 
+# Lecture / Lab Section: TC1L / TL3L 
+# Trimester: 2430 
+# Names: WESLEY_LIM_KHAI | MEMBER_NAME_2 | MEMBER_NAME_3 | MEMBER_NAME_4 
+# IDs: MEMBER_ID_1 | MEMBER_ID_2 | MEMBER_ID_3 | MEMBER_ID_3 
+# Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 | MEMBER_EMAIL_3 
+# *************************************************************************
